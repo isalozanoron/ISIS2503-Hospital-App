114f3fa68fa535f5458d69231f9706fdd324baa1
Mateo Cante <m.canteg111@gmail.com>
2025-03-13 13:30:20 -0500

    CAMBIOS MRI

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -20,6 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('', include('MRI.urls')),
     path('', include('cliente.urls')),
     path('', include('measurements.urls')),
     path('', include('variables.urls')),
