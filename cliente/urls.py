c04990adc860e1c03f5190f420b7bd483bfa6461
Mateo Cante <m.canteg111@gmail.com>
2025-03-13 12:19:53 -0500

    Cambios_url_cliente

--- cliente/urls.py
+++ cliente/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('clientes/', views.cliente_list, name='clienteList'),
+    path('cliente/', views.cliente_list, name='clienteList'),
     path('clientecreate/', csrf_exempt(views.cliente_create), name='clienteCreate'),
 ]
