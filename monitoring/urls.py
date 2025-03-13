becbd5659f8f99f0c6627f009f47faa516472205
Mateo Cante <m.canteg111@gmail.com>
2025-03-13 12:05:25 -0500

    Cambhios url cliente

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -20,6 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('', include('clientes.urls')),
     path('', include('measurements.urls')),
     path('', include('variables.urls')),
 ]
